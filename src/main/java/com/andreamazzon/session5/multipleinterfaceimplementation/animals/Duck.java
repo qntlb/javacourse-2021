bb2907baddcd4912016fb76b286f5fc9bba4ae24
Andrea Mazzon <mazzon@math.lmu.de>
2021-04-14 21:57:03 +0200

    Corrected a typo

--- src/main/java/com/andreamazzon/session5/multipleinterfaceimplementation/animals/Duck.java
+++ src/main/java/com/andreamazzon/session5/multipleinterfaceimplementation/animals/Duck.java
@@ -1,9 +1,9 @@
 package com.andreamazzon.session5.multipleinterfaceimplementation.animals;
 
-
 /**
- * Multiple implementation: this class implements both the interfaces, and then both the methods.
- * Note: multiple inheritance not allowed with (even abstract) classes
+ * Multiple implementation: this class implements both the interfaces, and then
+ * both the methods. Note: multiple inheritance not allowed with (even abstract)
+ * classes
  *
  * @author Andrea Mazzon
  *
@@ -11,18 +11,18 @@ package com.andreamazzon.session5.multipleinterfaceimplementation.animals;
 public class Duck implements AnimalThatSwims, AnimalThatFlies {
 
 	@Override
-	public void swim() {//methods defined in the interface have to be public!
+	public void swim() {// methods defined in the interface have to be public!
 		System.out.println("A duck swims");
 	}
 
 	@Override
-	public void fly() {//methods defined in the interface have to be public!
+	public void fly() {// methods defined in the interface have to be public!
 		System.out.println("A duck flies");
 	}
 
 	@Override
-	public void eat() {//methods defined in the interface have to be public!
+	public void eat() {// methods defined in the interface have to be public!
 		System.out.println("A duck eats");
-		//System.out.println(sameField);//POSSIBLE CONFLICT!
+		// System.out.println(interfaceField);//POSSIBLE CONFLICT!
 	}
 }
