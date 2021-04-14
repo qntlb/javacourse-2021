a838eec69becb4fccbf6cf62e3745149c9fb1d47
Andrea Mazzon <mazzon@math.lmu.de>
2021-04-14 15:06:12 +0200

    Method getRow moved up

--- src/main/java/com/andreamazzon/session4/usefulmatrices/UsefulMethodsMatricesVectors.java
+++ src/main/java/com/andreamazzon/session4/usefulmatrices/UsefulMethodsMatricesVectors.java
@@ -14,17 +14,27 @@ public class UsefulMethodsMatricesVectors {
 	/**
 	 *
 	 * @param matrix
+	 * @param columnIndex
+	 * @return it returns the row of matrix indicated by columnIndex
+	 */
+	public static double[] getRow(double[][] matrix, int rowIndex) {
+		return matrix[rowIndex];
+	}
+	
+	/**
+	 *
+	 * @param matrix
 	 * @return transpose of matrix
 	 */
 	public static double[][] getTranspose(double matrix[][]) {
-		int columnLength = matrix.length;
+		int numberOfRows = matrix.length; 
 		// matrix[0] is the first row of the matrix
-		int rowLength = matrix[0].length;// number of columns: length of the row
-		double[][] transpose = new double[rowLength][columnLength];
+		int numberOfColumns = matrix[0].length;// number of columns: length of the row
+		double[][] transpose = new double[numberOfColumns][numberOfRows];
 
 		// double for loop to transpose the matrix
-		for (int i = 0; i < rowLength; i++) {
-			for (int j = 0; j < columnLength; j++) {
+		for (int i = 0; i < numberOfColumns; i++) {
+			for (int j = 0; j < numberOfRows; j++) {
 				transpose[i][j] = matrix[j][i];
 			}
 		}
@@ -40,16 +50,6 @@ public class UsefulMethodsMatricesVectors {
 	public static double[] getColumn(double[][] matrix, int columnIndex) {
 		double[][] transpose = getTranspose(matrix);
 		return transpose[columnIndex];// the row of the transpose indicated by columnIndex
-	}
-
-	/**
-	 *
-	 * @param matrix
-	 * @param columnIndex
-	 * @return it returns the row of matrix indicated by columnIndex
-	 */
-	public static double[] getRow(double[][] matrix, int rowIndex) {
-		return matrix[rowIndex];
 	}
 
 	/**
