e9e04d67019fcb301736df7373eb47b9ee7d8c70
Andrea Mazzon <mazzon@math.lmu.de>
2021-04-17 13:20:48 +0200

    Some changes to the comments

--- src/main/java/com/andreamazzon/session5/interfacesandpolymorphism/band/Band.java
+++ src/main/java/com/andreamazzon/session5/interfacesandpolymorphism/band/Band.java
@@ -10,26 +10,33 @@ package com.andreamazzon.session5.interfacesandpolymorphism.band;
  *
  */
 public class Band {
+	/*
+	 * Polymorphism: at compilation time, this method doesn’t care about the type of
+	 * the instrument, so we can give it directly an Instrument reference. In this
+	 * way, when we add new types to the system it still works right, without any
+	 * need to change the code.
+	 */
 	/**
-	 * Polymorphism: at compilation time, it doesn’t care about the type of the
-	 * instrument, so we can give it directly an Instrument reference. In this way,
-	 * when we add new types to the system it still works right, without any need to
-	 * change the code.
+	 * It makes an Instrument playing a Note in its specific way
 	 */
 	public static void tune(Instrument i, Note n) {
 		i.play(n);
 	}
 
 	public static void main(String[] args) {
-		// Upcasting during addition to the array:
+		/*
+		 * The reference is always the one to the interface, but the specific type
+		 * changes. As in the example with shapes!
+		 */
 		Instrument[] orchestra = { new Wind(), new Percussion(), new Woodwind() };
 		Note[] notes = { Note.A, Note.F_SHARP, Note.B };
 
-		orchestra[0].play(Note.A);
+		orchestra[0].play(notes[0]);
 		System.out.println();
 
 		// cannot do it, even if it is a Wind(): the reference is to Instrument!
 		// orchestra[0].specificWind();
+
 		for (Instrument i : orchestra) {// look at the use of the foreach syntax!
 			for (Note n : notes) {
 				tune(i, n);
