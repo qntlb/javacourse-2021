66eacb1d27a5df660d5888cb3bae47a7c3021374
Andrea Mazzon <mazzon@math.lmu.de>
2021-04-23 23:19:23 +0200

    Added useful methods for exercise handout 2

--- src/main/java/com/andreamazzon/session4/usefulmatrices/UsefulMethodsMatricesVectors.java
+++ src/main/java/com/andreamazzon/session4/usefulmatrices/UsefulMethodsMatricesVectors.java
@@ -20,14 +20,15 @@ public class UsefulMethodsMatricesVectors {
 	public static double[] getRow(double[][] matrix, int rowIndex) {
 		return matrix[rowIndex];
 	}
-	
+
 	/**
 	 *
 	 * @param matrix
 	 * @return transpose of matrix
 	 */
 	public static double[][] getTranspose(double matrix[][]) {
-		int numberOfRows = matrix.length; 
+
+		int numberOfRows = matrix.length;
 		// matrix[0] is the first row of the matrix
 		int numberOfColumns = matrix[0].length;// number of columns: length of the row
 		double[][] transpose = new double[numberOfColumns][numberOfRows];
@@ -66,16 +67,81 @@ public class UsefulMethodsMatricesVectors {
 		return sum / length;
 	}
 
-	
 	/**
 	 * it prints the entries of vector
 	 *
 	 * @param vector
 	 */
 	public static void printVector(double[] vector) {
-		for (int i = 0; i < vector.length; i++) {
-			System.out.println(vector[i]);
+		for (double element : vector) {
+			System.out.println(element);
 		}
+	}
+
+	/**
+	 * It returns the biggest element of a one-dimensional array of doubles
+	 *
+	 * @param vector the one-dimensional array
+	 * @return the biggest element of the one-dimensional array
+	 */
+	public static double getMin(double[] vector) {
+		double min = vector[0];
+		for (int i = 1; i < vector.length; i++) {
+			if (vector[i] < min) {
+				min = vector[i];
+			}
+		}
+		return min;
+	}
+
+	/**
+	 * It returns the biggest element of a one-dimensional array of doubles
+	 *
+	 * @param vector the one-dimensional array
+	 * @return the biggest element of the one-dimensional array
+	 */
+	public static double getMax(double[] vector) {
+		double max = vector[0];
+		for (int i = 1; i < vector.length; i++) {
+			if (vector[i] > max) {
+				max = vector[i];
+			}
+		}
+		return max;
+	}
+
+	/**
+	 * It returns an array of integers which represent the number of realizations of
+	 * a given array that lie in every subinterval (bin) of an interval [minBin,
+	 * maxBin]. All the subintervals are of equal length. The first entry of the
+	 * integer array represent the number of realizations strictly smaller then
+	 * minBin, the last one the number of realizations bigger than maxBin.
+	 *
+	 * @param realisations
+	 * @param minBin
+	 * @param maxBin
+	 * @param binsNumber,  number of the subintervals
+	 * @return array of integers
+	 */
+	public static int[] buildHistogram(double[] realizations, double minBin, double maxBin, int binsNumber) {
+		int[] bins = new int[binsNumber + 2];// two more because of outliers
+		double binSize = (maxBin - minBin) / binsNumber; // every bin has the same length
+		for (double realization : realizations) {
+			if (realization < minBin) { // it goes in the first bin
+				bins[0] += 1;
+			} else if (realization > maxBin) {
+				bins[binsNumber + 1] += 1; // it goes in the last bin
+			} else {
+				int intRatio = (int) ((realization - minBin) / binSize);
+				/*
+				 * bin[i+1] (remember: in Java the first entry is 0, and here bin[0] hosts the
+				 * realization smaller than min) hosts the realization such that (realization -
+				 * minBin) / binSize is in [i,i+1)
+				 */
+				bins[intRatio + 1] += 1;
+			}
+		}
+		return bins;
 	}
 
 }
